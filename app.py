3eedfc7493ff753fb2a9dddb5a52d1e84dfae28f
VRamazing <r.vignesh149@gmail.com>
2017-07-10 13:37:57 +0530

    Posts added

---
+++ app.py
@@ -0,0 +1,10 @@
+from models.posts import Post
+
+__author__ = "vignesh Ramesh"
+
+post1 = Post("First","This is the first post","Vignesh Ramesh")
+post2 = Post("Second","This is the second post","Vignesh Ramesh")
+
+print(post1.content)
+
+print(post2.content)
