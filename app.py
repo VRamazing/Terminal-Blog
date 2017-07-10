db1a8173108faf6d34dbc4bbe751b8eeeed1c7bb
VRamazing <r.vignesh149@gmail.com>
2017-07-10 13:57:00 +0530

    Database.py added

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from models.posts import Post
+from database import Database
 
 __author__ = "vignesh Ramesh"
+
+Database.initialize()
 
 post1 = Post("First","This is the first post","Vignesh Ramesh")
 post2 = Post("Second","This is the second post","Vignesh Ramesh")
